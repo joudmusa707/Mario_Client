0bbc1ed5be78bc6d968c48e66178bcc5c42bd98d
joudmusa707 <ID+username@users.noreply.github.com>
2026-06-02 16:12:24 +0300

    done with level page

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,17 @@ import SignUp from "./pages/SignUpPage/SignUp.jsx";
 import GameUI from "./pages/GamePage/GameUI.jsx";
 import { Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import LevelSelection from "./pages/LevelSelectionPage/LevelSelection.jsx";
 function App() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const savedUser = localStorage.getItem("user");
     if (savedUser) {
       setUser(JSON.parse(savedUser));
     }
+    setLoading(false);
   }, []);
 
   const handleAuth = (userData) => {
@@ -22,6 +25,9 @@ function App() {
     localStorage.setItem("user", JSON.stringify(userData));
   };
 
+  if (loading) {
+    return <div className="text-white text-center mt-5">Loading app...</div>;
+  }
   return (
     <>
       {/*My Routes*/}
@@ -31,16 +37,30 @@ function App() {
           <Route
             path="/Login"
             element={
-              user ? <Navigate to="/Game" /> : <Login onLogin={handleAuth} />
+              user ? (
+                <Navigate to="/LevelSelection" />
+              ) : (
+                <Login onLogin={handleAuth} />
+              )
             }
           />
           <Route
             path="/SignUp"
             element={
-              user ? <Navigate to="/Game" /> : <SignUp onSignUp={handleAuth} />
+              user ? (
+                <Navigate to="/LevelSelection" />
+              ) : (
+                <SignUp onSignUp={handleAuth} />
+              )
             }
           />
           <Route path="/Game" element={<GameUI />} />
+          <Route
+            path="/LevelSelection"
+            element={
+              user ? <LevelSelection user={user} /> : <Navigate to="/Login" />
+            }
+          />
         </Routes>
       </Router>
     </>
